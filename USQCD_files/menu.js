326dcd193cb6150331edb816cf0379f97c084dbb
Robert Edwards <edwards@jlab.org>
2014-01-22 15:26:16 -0500

    Slurped in Oliver Witzel's "www" repo.

---
+++ USQCD_files/menu.js
@@ -0,0 +1,54 @@
+document.write('<center><table class="menu">');
+document.write('<tr><th  colspan="2"> <a href="./USQCD.html"> SOFTWARE</a> </th></tr>');
+document.write('<tr><th  colspan="2"><a href="./Tutorials.html"> Tutorials </a></th></tr>');
+document.write('<tr><th  colspan="2"> <a href="./References.html"> References</a> </th></tr>');
+document.write('<tr><th  class="alt" colspan="2"> Applications </th></tr>');
+document.write('<tr> ');
+document.write('    <td><a href="http://usqcd.jlab.org/usqcd-docs/chroma/">Chroma</a></td>');
+document.write('    <td><a href="http://www.physics.utah.edu/~detar/milc/index.html">MILC</a></td>');
+document.write('</tr>');
+document.write('<tr>');
+document.write('    <td><a href="http://qcdoc.phys.columbia.edu/cps.html">CPS</a></td>');
+document.write('    <td><a href="https://usqcd.lns.mit.edu/redmine/projects/build">QLUA</a></td>');
+document.write('</tr>');
+document.write('<tr>');
+document.write('    <td><a href="./FUEL.html">FUEL</a></td>');
+document.write('    <td><a href="http://lattice.github.com/quda">QUDA</a></td>');
+document.write('</tr>');
+
+document.write('<tr><th  class="alt" colspan="2"> Level 3 libraries </th></tr>');
+document.write('<tr>');
+document.write('    <td><a href="http://usqcd.jlab.org/usqcd-docs/qopqdp">QOPQDP</a></td>');
+document.write('    <td><a href=" https://usqcd.lns.mit.edu/redmine/projects/clover">CLOVER</a><br></td>');
+document.write('</tr>');
+document.write('<tr>');
+document.write('    <td><a href="https://usqcd.lns.mit.edu/redmine/projects/mdwf">MDWF</a><br></td>');
+document.write('    <td><a href="http://www.mit.edu/~avp/sse/">CG DWF</a></td>');
+document.write('</tr>');
+
+document.write('<tr><th  class="alt" colspan="2"> Level 2 libraries </th></tr>');
+document.write('<tr>');
+document.write('    <td><a href="http://usqcd.jlab.org/usqcd-docs/qdp++/">QDP/C++</a></td>');
+document.write('    <td><a href="http://usqcd.jlab.org/usqcd-docs/qio">QIO</a></td>');
+document.write('</tr>');
+document.write('<tr>');
+document.write('    <td><a href="http://usqcd.jlab.org/usqcd-docs/qdp/">QDP/C</a></td>');
+document.write('    <td><a href="http://usqcd.jlab.org/usqcd-docs/c-lime">C-LIME</a></td>');
+document.write('</tr>');
+document.write('<tr>');
+document.write('    <td><a href="http://usqcd.jlab.org/usqcd-docs/bagel_qdp/">BAGEL QDP</a></td>');
+document.write('</tr>');
+
+document.write('<tr><th  class="alt" colspan="2"> Level 1 libraries </th></tr>');
+document.write('<tr>');
+document.write('    <td><a href="http://usqcd.jlab.org/usqcd-docs/qla">QLA</a></td>');
+document.write('    <td><a href="http://usqcd.jlab.org/usqcd-docs/qmp">QMP</a></td>');
+document.write('</tr>');
+document.write('<tr>');
+document.write('    <td><a href="http://usqcd.jlab.org/usqcd-docs/qmt">QMT</a></td>');
+document.write('</tr>');
+
+document.write('<tr><th  colspan="2"> <a href="./MoreSoftware.html"> More Software</a> </th></tr>');
+document.write('<tr><th  colspan="2"> <a href="./GaugeConnection.html"> Gauge Connection</a> </th></tr>');
+document.write('<tr><th  colspan="2"> <a href="./Hardware.html"> Hardware</a> </th></tr>');
+document.write('</table></center>');
